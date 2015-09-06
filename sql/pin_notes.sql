b4bf966254e961dea647aff8a1cc01d3fd56bfca
kpatel1989 <katk.1989@gmail.com>
2015-09-06 18:14:24 +0530

    updated database

--- sql/pin_notes.sql
+++ sql/pin_notes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2015 at 09:30 PM
+-- Generation Time: Sep 06, 2015 at 02:43 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,6 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `adminId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `admin` (`adminId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `adminId`) VALUES
+(2, 'asd', 1),
+(3, '123', 1),
+(4, '2222grp', 3),
+(5, 'a sdasdasd', 3),
+(6, 'asd', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `notes`
 --
 
@@ -30,24 +55,24 @@ CREATE TABLE IF NOT EXISTS `notes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(45) DEFAULT NULL,
   `description` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `userId` int(11) DEFAULT NULL,
+  `groupId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  KEY `groupId` (`groupId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 --
 -- Dumping data for table `notes`
 --
 
-INSERT INTO `notes` (`id`, `title`, `description`) VALUES
-(1, 'asd', 'awd'),
-(2, 'asd', 'awd'),
-(3, 'asd', 'awd'),
-(4, 'asd', 'awdawd'),
-(5, 'asd', 'awdawdawdad'),
-(6, 'aw daw d', 'ad awdawd'),
-(7, 'aw daw d', 'ad awdawd'),
-(8, 'asdw', 'awdawd'),
-(9, 'asdw', 'awdawd'),
-(10, 'adwawd', 'awdawd');
+INSERT INTO `notes` (`id`, `title`, `description`, `userId`, `groupId`) VALUES
+(22, 'asdasd', 'adwdawd', 1, NULL),
+(23, 'ert', 'zcx', 1, NULL),
+(24, '222', 'asdsd', 3, NULL),
+(25, 'asd grp', 'desc', 1, 2),
+(26, '123 title', 'desc', 1, 3),
+(27, '123 t1', 'asdasd', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -74,6 +99,23 @@ INSERT INTO `user` (`id`, `emailId`, `password`) VALUES
 (5, 'asd@ad.asc', 'asd'),
 (6, 'asd@qwe.df', '123'),
 (7, 'asd@qwe.df123123', 'asd');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `fk_group_admin_id` FOREIGN KEY (`adminId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `fk_group_notes` FOREIGN KEY (`groupId`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_notes` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
